5c50fab3ed4d467b015a08e2f53f1632c8b4d894
NiveditaBhagat <bhagatnivedita469@gmail.com>
2026-03-23 23:18:11 +0530

    added env config and gitignore

--- app/database.py
+++ app/database.py
@@ -1,13 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-
-SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db'  # this url is used to create location of this database on our fastAPI application. 
+from dotenv import load_dotenv
+import os
+# SQLALCHEMY_DATABASE_URL='sqlite:///./todosapp.db'  # this url is used to create location of this database on our fastAPI application. 
 # Our database is going to be inside this directory of our to do app.
 
 # databse engine is something that we can use to be able to open up a connection and be able to use our database.
+load_dotenv()
 
-engine=create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+SQLALCHEMY_DATABASE_URL=os.getenv("DATABASE_URL")
+print(SQLALCHEMY_DATABASE_URL)
+engine=create_engine(SQLALCHEMY_DATABASE_URL)
 
 # create a session local and each instance of the session local will have a database session.The class itself is not a database session yet.
 
