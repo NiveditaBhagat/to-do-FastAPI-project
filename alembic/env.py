83a3482c48c9c6ae533c12a0d1a0906a824a4d92
NiveditaBhagat <bhagatnivedita469@gmail.com>
2026-03-28 01:18:54 +0530

    alembic upgrade phone number

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import os
 from alembic import context
 
+from app import models
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -28,7 +30,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = models.Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- alembic/versions/f390520760f0_create_phone_number_for_user_column.py
+++ alembic/versions/f390520760f0_create_phone_number_for_user_column.py
@@ -19,8 +19,8 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    """Upgrade schema."""
-    pass
+    op.add_column('users',sa.Column('phone_number',sa.String(), nullable=True))
+    
 
 
 def downgrade() -> None:
