501a2c3efce3e7e5fa114a5749b379fb8c7ceeba
NiveditaBhagat <bhagatnivedita469@gmail.com>
2026-03-27 23:19:21 +0530

    alembic upgrde revision

--- alembic/env.py
+++ alembic/env.py
@@ -21,8 +21,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+# if config.config_file_name is not None:
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
---
+++ alembic/versions/f390520760f0_create_phone_number_for_user_column.py
@@ -0,0 +1,28 @@
+"""Create phone number for user column
+
+Revision ID: f390520760f0
+Revises: 
+Create Date: 2026-03-27 23:18:51.658836
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f390520760f0'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
